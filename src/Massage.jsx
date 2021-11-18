5f70b45fa0b192eac719734a341436052c050a83
kuznetsov_a_g <skuznetsov92@mail.ru>
2021-11-18 13:30:06 +0300

    home: lesson_1

---
+++ src/Massage.jsx
@@ -0,0 +1,19 @@
+import "./Massage.css";
+
+export default function Massage({ road }) {
+  return (
+    <div>
+      <h2>Company {road}</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque numquam
+        illum reiciendis sapiente libero dolor temporibus qui saepe error
+        repellat. Ipsa, maxime. Odio eos dolore nulla veritatis corrupti,
+        dolorum eligendi?
+      </p>
+      <h3>
+        <i>{road}</i>
+      </h3>
+    </div>
+  );
+}
+ 
