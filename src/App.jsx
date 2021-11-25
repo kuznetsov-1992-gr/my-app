de4a69a915a58056a5953622a9d07eb78bd0d366
kuznetsov_a_g <skuznetsov92@mail.ru>
2021-11-25 22:27:47 +0300

    lesson_3

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import React from "react";
 // import logo from './logo.svg';
 // import "./Massage.css";
 import "./App.css";
 // import Monaco from './img/Monaco.jpg'
 import { MassageList } from "./Components";
+import {ChatList} from "./Components"
+
 
 function App() {
   
   return (
     <div className="App">
-      <MassageList></MassageList>
-      
+      <div className="flex-racurs">
+        <ChatList></ChatList>
+        <MassageList></MassageList>
+      </div>
     </div>
   );
 }
